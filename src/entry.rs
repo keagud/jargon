8c88f68927a7803ad17c6d47c1143457d8908202
keagud <keagud@protonmail.com>
2023-02-15 18:41:15 -0800

    getting the swing of this rust thing

--- src/entry.rs
+++ src/entry.rs
@@ -1,6 +1,16 @@
 mod entry {
 
+    use std::borrow::Cow;
+
+    struct Entry {
+        title: Cow<'static, str>,
+        pos: Cow<'static, str>,
+        definitions: Vec<Cow<'static, str>>,
+    }
 
 
 
+    fn tost(){
+    let e = Entry{title: "yangle", pos: "N", vec![]};
+    }
 }
