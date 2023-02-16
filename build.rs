1c1cce1deb1ff293f47e65bd3ad81209bfe9dbdb
keagud <keagud@protonmail.com>
2023-02-16 14:13:33 -0800

    slouching toward a working build script

--- build.rs
+++ build.rs
@@ -1,3 +1,31 @@
+use serde::Deserialize;
+use serde_json;
+use std::convert::AsRef;
+use std::fs::File;
+use std::io;
+use std::path::Path;
 
-use serde_json::Value;
-fn main() {}
+#[derive(Deserialize)]
+struct EntryFields {
+    title: String,
+    pos: String,
+    content: String,
+}
+
+fn read_entries<P: AsRef<Path>>(json_path: P) -> io::Result<Vec<EntryFields>> {
+    let file_handle = File::open(json_path)?;
+    let reader = io::BufReader::new(file_handle);
+    let data: Vec<EntryFields> = serde_json::from_reader(reader)?;
+
+    Ok(data)
+}
+
+fn main() -> io::Result<()> {
+    let res = read_entries("jargon.json")?;
+
+    for r in res {
+        println!("{}", r.title);
+    }
+
+    Ok(())
+}
