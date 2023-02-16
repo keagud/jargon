f7f8d444f383e4263e01a7b1b35343cff23cd3a2
keagud <keagud@protonmail.com>
2023-02-16 15:50:04 -0800

    sometimes you get blinded by macros, when the std::fmt was right in front of you

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 use serde::Deserialize;
 use serde_json;
 use std::convert::AsRef;
+use std::format;
 use std::fs::File;
-use std::io;
+use std::io::prelude::*;
 use std::path::Path;
 
 #[derive(Deserialize)]
@@ -12,20 +13,57 @@ struct EntryFields {
     content: String,
 }
 
-fn read_entries<P: AsRef<Path>>(json_path: P) -> io::Result<Vec<EntryFields>> {
+impl EntryFields {
+    fn expand_code(&self) -> String {
+        /* format!(
+            "make_entry!(r\"{}\",\"{}\",r#\"{}\"#),\n",
+            self.title, self.pos, self.content
+        )*/
+
+        format!(
+            "[\nr\"{}\", \"{}\",r#####\"{}\"#####\n],\n\n",
+            self.title, self.pos, self.content
+        )
+    }
+}
+
+fn read_entries<P: AsRef<Path>>(json_path: P) -> std::io::Result<Vec<EntryFields>> {
     let file_handle = File::open(json_path)?;
-    let reader = io::BufReader::new(file_handle);
+    let reader = std::io::BufReader::new(file_handle);
     let data: Vec<EntryFields> = serde_json::from_reader(reader)?;
 
     Ok(data)
 }
 
-fn main() -> io::Result<()> {
-    let res = read_entries("jargon.json")?;
+fn write_to_file<P: AsRef<Path>>(
+    entries: &Vec<EntryFields>,
+    target_path: P,
+) -> std::io::Result<()> {
+    let entries_count = entries.len();
+    let mut file_handle = File::create(&target_path)?;
 
-    for r in res {
-        println!("{}", r.title);
+    writeln!(&mut file_handle, "//Macros? You mean format strings?")?;
+
+    //const TD: [[&str; 3]; 2] = [["foo", "", "bar"], ["baz", "", "boz"]];
+    writeln!(
+        &mut file_handle,
+        "const ENTRIES: [[&str; 3]; {}] = [",
+        entries_count
+    )?;
+
+    for entry in entries {
+        file_handle.write_all(entry.expand_code().as_bytes())?;
     }
+
+    writeln!(&mut file_handle, "];")?;
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let res = read_entries("src/jargon.json")?;
+
+    write_to_file(&res, "src/gen_entries.rs")?;
 
     Ok(())
 }
