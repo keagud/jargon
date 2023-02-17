422be9dc24adad0e80e250c8911e664fd2ca5efc
keagud <keagud@protonmail.com>
2023-02-17 12:25:42 -0800

    rustier refactor of search logic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use gen_entries::ENTRIES as jargon_entries;
 use gen_entries::NUM_ENTRIES as entries_count;
 
 use std::env;
-use std::io;
 
 use colored::Colorize;
 use fuzzy_matcher::skim::SkimMatcherV2;
@@ -14,13 +13,17 @@ use textwrap;
 type Entry = [&'static str; 3];
 
 fn match_entry(query: &str) -> Option<Entry> {
-    let matcher = SkimMatcherV2::default();
-
-    let matcher = matcher.ignore_case();
+    let matcher = SkimMatcherV2::default().smart_case();
 
     let entry_iter = IntoIterator::into_iter(jargon_entries);
-
-    entry_iter.max_by_key(|e| matcher.fuzzy_match(query, e[0]))
+    match entry_iter
+        .map(|e| (matcher.fuzzy_match(e[0], query).unwrap_or(0), e))
+        .max_by_key(|t| t.0)
+        .expect("The entries sequence should never be empty")
+    {
+        (0, ..) => None,
+        (.., v) => Some(v),
+    }
 }
 
 fn print_entry(e: Entry, opts: textwrap::Options) {
@@ -45,21 +48,21 @@ fn random_entry() -> Entry {
     jargon_entries[rand_value]
 }
 
-fn main() -> io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = if args.len() > 1 { Some(&args[1]) } else { None };
 
-    let query_result = match query {
+    if let Some(query_result) = match query {
         Some(q) => match_entry(q),
         None => Some(random_entry()),
+    } {
+        let opts = textwrap::Options::new(80)
+            .initial_indent("  ")
+            .subsequent_indent("    ");
+
+        print_entry(query_result, opts);
+    } else {
+        println!("{}", "No match found!".bold());
     }
-    .ok_or(io::ErrorKind::NotFound)?;
-
-    let opts = textwrap::Options::new(80)
-        .initial_indent("  ")
-        .subsequent_indent("    ");
-
-    print_entry(query_result, opts);
-    Ok(())
 }
