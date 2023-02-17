80bcb64d8181e0e7c72defb3150754b62f21b1a5
keagud <keagud@protonmail.com>
2023-02-16 20:50:42 -0800

    add proper formatting and text wrap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod gen_entries;
 use gen_entries::ENTRIES as jargon_entries;
 
+use colored::Colorize;
 use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
+use textwrap::fill;
 
-fn match_entry(query: &str, threshold: i64) -> Option<[&str; 3]> {
+type Entry = [&'static str; 3];
+
+fn match_entry(query: &str, threshold: i64) -> Option<Entry> {
     let matcher = SkimMatcherV2::default();
 
     let entry_iter = IntoIterator::into_iter(jargon_entries);
@@ -20,11 +24,22 @@ fn match_entry(query: &str, threshold: i64) -> Option<[&str; 3]> {
     }
 }
 
+fn format_entry(e: Entry, width: usize) -> String {
+    fill(
+        match e {
+            [title, "", content] => format!("{}\n{}", title.bold().blue(), content),
+            [title, pos, content] => {
+                format!("{} {}\n{}", title.bold().blue(), pos.italic().magenta(), content)
+            }
+        }
+        .as_str(),
+        width,
+    )
+}
+
 fn main() {
     if let Some(s) = match_entry("zorkmid", 0) {
-        let desc = s[2];
-
-        println!("{}", desc);
+        println!("{}", format_entry(s, 80));
     } else {
         println!("That didn't work out")
     };
